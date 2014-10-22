fa0bc06320c7b67de35cddaba453a9691c771ed7
Mike Fikes <mike@fikesfarm.com>
2014-10-22 08:27:08 -0400

    Code formatting

--- iOS/src/GBYAlertUtils.m
+++ iOS/src/GBYAlertUtils.m
@@ -5,12 +5,11 @@
 + (void)alertWithMessage:(NSString*)message
 {
     dispatch_async(dispatch_get_main_queue(), ^{
-        UIAlertView *alert = [[UIAlertView alloc]
-                              initWithTitle:nil
-                              message				:message
-                              delegate			:nil
-                              cancelButtonTitle	:NSLocalizedString(@"OK", nil)
-                              otherButtonTitles	:nil];
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil
+                                                        message:message
+                                                       delegate:nil
+                                              cancelButtonTitle:NSLocalizedString(@"OK", nil)
+                                              otherButtonTitles:nil];
         [alert show];
     });
 }
