c148226c0555c0869a3aaff738a9ead37d596dcd
Mike Fikes <mike@fikesfarm.com>
2014-10-22 12:45:33 -0400

    Documentation

--- iOS/src/GBYTextField.h
+++ iOS/src/GBYTextField.h
@@ -8,17 +8,54 @@
  */
 @protocol GBYTextField <JSExport>
 
+/**
+ Maps to the `enabled` property.
+ */
 @property JSValue* enabled;
+
+/**
+ Maps to the `hidden` property.
+ */
 @property JSValue* hidden;
+
+/**
+ Maps to the `text` property.
+ */
 @property (nonatomic) NSString* text;
+
+/**
+ Maps to the `placeholder` property.
+ */
 @property (nonatomic) NSString* placeholder;
+
+/**
+ Maps to the `textColor` property.
+ */
 @property (nonatomic) GBYColor* textColor;
 
+/**
+ Sets a callback for `UIControlEventEditingDidBegin`.
+ */
 -(void)setEditingDidBeginCallback:(JSValue*)callback;
+
+/**
+ Sets a callback for `UIControlEventEditingChanged`.
+ */
 -(void)setEditingChangedCallback:(JSValue*)callback;
+
+/**
+ Sets a callback for `UIControlEventEditingDidEnd`.
+ */
 -(void)setEditingDidEndCallback:(JSValue*)callback;
+
+/**
+ Sets a callback for `UIControlEventEditingDidEndOnExit`.
+ */
 -(void)setEditingDidEndOnExitCallback:(JSValue*)callback;
 
+/**
+ Maps to the `becomeFirstResponder` method.
+ */
 -(void)becomeFirstResponder;
 
 @end
@@ -28,6 +65,12 @@
  */
 @interface GBYTextField : NSObject<GBYTextField>
 
+/**
+ Wraps a text field for interop.
+ 
+ @param textField The text field to wrap.
+ @return A wrapped text field.
+ */
 +(GBYTextField*)wrap:(UITextField*)textField;
 
 @end
