3e335ed69a2e3bd238b81dfe7b9c13d700f110ab
Mike Fikes <mike@fikesfarm.com>
2014-10-22 09:06:41 -0400

    Add docstrings for the macros.

--- ClojureScript/src/goby/macros.clj
+++ ClojureScript/src/goby/macros.clj
@@ -1,11 +1,22 @@
 (ns goby.macros)
 
-(defmacro reify [protocol & impls]
+(defmacro reify
+  "Implements a protocol, delegating to clojure.core/reify while also adding an
+  additional method named <protocol>-implements which indicates the protocol
+  methods implemented. This is used to partially implement a protocol which mimics
+  an Objective-C protocol where some of the methods are optional. The additional
+  method <protocol>-implements is used at runtime to determine which methods
+  are actually implemented."
+  [protocol & impls]
   `(clojure.core/reify ~protocol
      (~(symbol (str protocol "-implements")) [~(symbol "_")] (cljs.core/clj->js ~(mapv (comp str first) impls)))
      ~@impls))
 
 (defmacro defui [& ui-elements]
+  "Given a list of ui-elements, defines an atom per ui-element, along with an implicit
+  view-controller atom. Additionally exports a view-did-load! method which initializes
+  the atoms with the supplied arguments. Then calls a handle-view-did-load! method to
+  allow the namespace to perform further initialization when the view loads."
   (let [my-ns (str *ns*)]
     `(do
        (def ~'view-controller (atom nil))
