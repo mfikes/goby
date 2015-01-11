7c5b5cb506a424a3cf8b430c52f884c201abc1e5
Mike Fikes <mike@fikesfarm.com>
2015-01-10 21:00:31 -0500

    Prepare for release as a lib

---
+++ ClojureScript/project.clj
@@ -0,0 +1,13 @@
+(defproject goby "0.1.0-SNAPSHOT"
+  :description "Develop iOS apps in ClojureScript"
+  :url "http://github.com/mfikes/goby"
+  :license {:name "Eclipse"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+ 
+  :source-paths  ["src"]
+ 
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2511" :scope "provided"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha" :scope "provided"]]
+ 
+  :plugins [[lein-cljsbuild "1.0.3"]])
